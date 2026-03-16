a4b558ab7c60634e853d08d265c5fb9484c83f5c
eldergsilva <eldergsilva@gmail.com>
2026-03-16 10:19:58 -0300

    exercicio 01 Cadastro de usuário

---
+++ 01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: 'Denis Chaudet',
+    numero: 102,
+    complemento: "casa",
+    bairro: "Butantã",
+    cep: "05528-001",  
+    cidade: "São Paulo",
+    estado: "SP",
+    pais: "Brasil"
+};
+
+const usuario = {
+     nome: "Elder Gomes",
+     email: "eldergsilva@gmail.com",
+     telefone: "(11) 90552-8220",  
+     data_nascimento: "19/12/1984",  
+     endereco: endereco
+};
+
+console.log(usuario);
