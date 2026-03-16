4bca300566341cfa45931d3d2fbc11d4deec9d0e
eldergsilva <eldergsilva@gmail.com>
2026-03-16 19:41:42 -0300

    exercicio 02 Lista de veículos

---
+++ 02/index.js
@@ -0,0 +1,27 @@
+const carros = [
+    {
+        marca: "Toyota",
+        modelo: "Corolla",
+        ano: 2019,
+        cor: "Prata",
+        quantidade_portas: 4,
+        automatico: true
+    },
+    {
+        marca: "Honda",
+        modelo: "Civic",
+        ano: 2005,
+        cor: "Preto",
+        quantidade_portas: 4,
+        automatico: false
+    },
+    {
+        marca: "Volkswagen",
+        modelo: "Golf",
+        ano: 2011,
+        cor: "Branco",
+        quantidade_portas: 2,
+        automatico: false
+    }
+]
+console.log(carros);
